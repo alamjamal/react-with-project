1a407bce6839bf8cfeeccb5e1bc5f0d7efb6705c
alamjamal <alamjamal88@gmail.com>
2021-09-02 17:51:09 +0530

    hello commit

---
+++ alam/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+       <>
+            <h1>alam</h1>
+            <h2 className="alam">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab mollitia modi quaerat laudantium quasi aperiam hic, non ducimus ullam et odio alias corporis consequuntur nostrum. Ex iste quos veritatis aut.</h2>
+        </>
+    )
+}
+
+export default App
